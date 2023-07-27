9db41789c4c33dc01dd82f61998985624842325e
sanadevel <a36133353@gmail.com>
2023-07-27 11:16:08 +0900

    canvas add

--- src/components/canvas/style.jsx
+++ src/components/canvas/style.jsx
@@ -1,66 +1,78 @@
 import { styled } from "styled-components";
 
 export const Canvas = styled.div`
-text-align: center;
-width: 700px;
-margin: auto;
-  canvas{
+  text-align: center;
+  width: 1000px;
+  margin-top: 30px;
+  canvas {
     border: 1px solid black;
     background-color: #eeeeee;
   }
-  .div{
+
+  .div {
+    width: 1000px;
     display: flex;
+    justify-content: space-around;
     align-items: center;
     margin-top: 15px;
     border: 1px solid black;
     background-color: #eeeeee;
     height: 100px;
-    .tools{
-      .icons{
+    .tools {
+      cursor: pointer;
+      .icons {
         display: flex;
-        justify-content: center;
+        justify-content: space-around;
+        div {
+          padding: 1px;
+          border: 1px solid black;
+          border-radius: 3px;
+        }
       }
-      .slidebar{
-        input{
+      .slidebar {
+        input {
           width: 300px;
         }
       }
     }
-    .color{
+    .color {
       display: flex;
-      div{
-        width: 30px;
-        height: 30px;
+      justify-content: space-around;
+      div,
+      input {
+        cursor: pointer;
+        width: 50px;
+        height: 50px;
         margin-left: 10px;
         border-radius: 4px;
       }
-      .red{
-        background-color: #FF0000;
+      .red {
+        background-color: #ff0000;
       }
-      .orange{
+      .orange {
         background-color: orange;
       }
-      .yellow{
+      .yellow {
         background-color: yellow;
-      } 
-      .green{
+      }
+      .green {
         background-color: green;
-      } 
-      .skyblue{
+      }
+      .skyblue {
         background-color: skyblue;
       }
-      .navy{
+      .navy {
         background-color: navy;
       }
-      .purple{
+      .purple {
         background-color: purple;
       }
-      .black{
+      .black {
         background-color: black;
       }
-      .white{
+      .white {
         background-color: white;
       }
     }
   }
-`
+`;
